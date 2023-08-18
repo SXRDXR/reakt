55140fa9c5dd22a103ee4fdd21caad401a0b3d7b
SXRDXR <sardorbekboynzarov@gmail.com>
2023-08-18 17:57:19 +0500

    bir

---
+++ src/Components/Pages/PageOne/PageOne.jsx
@@ -0,0 +1,16 @@
+import React, {useEffect} from "react";
+import style from "./style.module.css";
+import Header from "../../Header/Header";
+
+export default function PageOne() {
+
+    useEffect(()=>{
+
+    },[])
+
+    return(
+        <div className={style.container}>
+
+        </div>
+    )
+}
